d6d47aec88d01c91f395426996194118c30dfc1b
Ngodi <albertngodi@gmail.com>
2020-04-04 08:26:32 +0100

    added header slogan

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.scss';
+
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="company-name">
+           AE WEALTH
+        </div>
+        <div className="slogan">
+        Financial Consulting.  Let Us Do the Math.
+        </div>
+
+    </div>
+  );
+}
+
+export default Header;
