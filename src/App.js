ba7b7969510e0979e22caf7a9d35dc52b13a71ce
Ngodi <albertngodi@gmail.com>
2020-04-04 10:47:00 +0100

    initial service heading animation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.scss';
 
 import Navigation from './components/Navigation/Navigation';
 import Header from './components/Header/Header';
+import Services from './components/Services/Services';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Header />
+      <Services />
     </div>
   );
 }
---
+++ src/components/Services/Services.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Services.scss';
+
+
+const Services = () => {
+  return (
+    <div className="services">
+        <div className="heading">
+        <i class="fas fa-10x fa-chart-pie"></i>
+            <span className="heading-text">OUR SERVICES</span>
+        </div>
+    </div>
+  );
+}
+
+export default Services;
