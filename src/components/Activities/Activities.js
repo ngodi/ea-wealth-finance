b8f3f1a212555707cebb4d278d2ed795c317d535
Ngodi <albertngodi@gmail.com>
2020-04-05 09:49:00 +0100

    react-slider installed

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -4,11 +4,19 @@ import './Activities.scss';
 import '../../styles/main.scss';
 
 const Activities = () => {
+
   return (
     <div className="activities">
         <div className="heading">
         <i class="fas fa-10x fa-hourglass-end"></i>
            <span className="heading-text">RECENT ACTIVITIES</span>
+        </div>
+        <div className="activities-content">
+            <div className="activity">
+                <div className="activity-name">Facebook-live Talk on Credit</div>
+                <div className="activity-date">Saturday 05 APRIL</div>
+                <button className="activity-link">More...</button>
+            </div>
         </div>
     </div>
   );
