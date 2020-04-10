25ee10ab2422c11ecf11fb19375748730775b247
ngodi albert <albertngodi@gmail.com>
2020-04-10 13:08:43 +0100

    setup responsive nav click actions

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Navigation.scss';
 
 
-const Navigation = () => {
-  return (
+class Navigation extends Component {
+  constructor(){
+    super();
+    this.state = {
+   checked: false
+};
+  }
+toggleChecked = () => {
+  this.setState({checked: !this.state.checked})
+}
+
+render(){
+ let output = (this.state.checked)?  <input type="checkbox" onClick={this.toggleChecked} className="navigation1__checkbox" id="navi-toggle" checked/>:
+<input type="checkbox" onClick={this.toggleChecked} className="navigation1__checkbox" id="navi-toggle"/>;
+   return (
   <div>
     <div className="navigation">
         <div className="logo">
@@ -19,7 +32,7 @@ const Navigation = () => {
         </div>
         </div>
         <div className="navigation1">
-          <input type="checkbox" className="navigation1__checkbox" id="navi-toggle" />
+         {output}
           <label htmlFor="navi-toggle" className="navigation1__button">
              <span className="navigation1__icon">&nbsp;</span>
           </label>
@@ -27,17 +40,19 @@ const Navigation = () => {
 
           <div className="navigation1__nav">
             <ul className="navigation1__list">
-              <li className="navigation1__item"><a className="navigation1__link" href="#home">HOME</a></li>
-              <li className="navigation1__item"><a className="navigation1__link" href="#services">SERVICES</a></li>
-              <li className="navigation1__item"><a className="navigation1__link" href="#about">ABOUT</a></li>
-              <li className="navigation1__item"><a className="navigation1__link" href="#activities">ACTIVITIES</a></li>
-              <li className="navigation1__item"><a className="navigation1__link" href="#contact">CONTACT</a></li>
+              <li className="navigation1__item"><a onClick={this.toggleChecked} className="navigation1__link" href="#home">HOME</a></li>
+              <li className="navigation1__item"><a onClick={this.toggleChecked} className="navigation1__link" href="#services">SERVICES</a></li>
+              <li className="navigation1__item"><a onClick={this.toggleChecked} className="navigation1__link" href="#about">ABOUT</a></li>
+              <li className="navigation1__item"><a onClick={this.toggleChecked} className="navigation1__link" href="#activities">ACTIVITIES</a></li>
+              <li className="navigation1__item"><a onClick={this.toggleChecked} className="navigation1__link" href="#contact">CONTACT</a></li>
             </ul>
           </div>
         </div>
 
     </div>
   );
+}
+ 
 }
 
 export default Navigation;
